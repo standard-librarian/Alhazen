ebd13076c4c4ffc6f73029b1a118b1dbc4a668ae
Medhat Mohammed <medhatmohammed1612@gmail.com>
2023-09-13 18:33:01 +0300

    Add Authentication using Clerk

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React from 'react';
 
 export default function DashboardPage() {
   return (
-    <div> DashboardPage
-    </div>
-  )
+    <>
+      <div> DashboardPage</div>
+    </>
+  );
 }
--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from 'react';
+
+import Link from 'next/link';
+
+import { Button } from '@/components/ui/button';
 
 export default function LandingPage() {
   return (
-    <div>LandingPage (unprotected)</div>
-  )
+    <div>
+      LandingPage (unprotected)
+      <div>
+        <Link href="/sign-in">
+          <Button> Login </Button>
+        </Link>
+        <Link href="/sign-up">
+          <Button> Register  </Button>
+        </Link>
+      </div>
+    </div>
+  );
 }
